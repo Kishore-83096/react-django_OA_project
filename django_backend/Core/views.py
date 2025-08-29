4279165372e8259716b151fdc7c2b2b45e1706ac
Kishore Siripurapu <kishore.siripurapu1484@gmail.com>
2025-08-29 22:44:16 +0530

    update the ui of result page and made some change to in submit view and stored detailed breakdown of the result

--- django_backend/Core/views.py
+++ django_backend/Core/views.py
@@ -229,13 +229,6 @@ def user_dashboard(request):
 # =========================================================
 #                EXAM FLOW
 # =========================================================
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from django.conf import settings
-import json
-from .models import ExamAttempt
-
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
